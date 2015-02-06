ff44c1a5cd76d87bc41081b4fad21be7d4bdba5b
绍辉 董 <dong@shaohuideMacBook-Pro.local>
2015-02-06 14:51:44 +0800

    新增对用户账户信息进行教研

--- app/models/hwm_user_account.rb
+++ app/models/hwm_user_account.rb
@@ -3,6 +3,22 @@
 # description : 用户账户model层
 class HwmUserAccount < ActiveRecord::Base
   
+  ## 数据校验层
+  validates :nickname, :presence => { :message => "用户昵称不能为空!" },
+  :uniqueness => { :message => "用户昵称不能重复!" },
+  :length => {:minimum => 6 , :maximum => 30}     # 用户昵称校验
+  
+  validates_format_of :email, :with => /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i,
+  :allow_nil => true     # 验证邮箱格式
+  
+  validates :mobile, :presence => { :message => "用户手机号码不能为空!" }, 
+  :uniqueness => { :message => "用户手机号码不能重复!" }  # 手机信息基本验证
+  
+  validates_format_of :mobile,
+  :with => /1\d{10}/   # 验证手机格式
+  
+  
+  
   ## 用户账户与标签建立联系
   has_many :hwm_user_account_hwm_labels # 与关联表建立对应关系
   has_many :hwm_labels, :through => :hwm_user_account_hwm_labels # 与标签表建立多对多关系
@@ -17,4 +33,6 @@ class HwmUserAccount < ActiveRecord::Base
 
   ## 用户账户与用户角色关联
   belongs_to :hwm_user_account_role
+  
+  
 end
--- app/models/hwm_user_account_role.rb
+++ app/models/hwm_user_account_role.rb
@@ -6,6 +6,7 @@ class HwmUserAccountRole < ActiveRecord::Base
   ## 校验层
   validates_presence_of :hwm_user_account_type, :message => "用户账户类型不可以为空!"
   validates_uniqueness_of :hwm_user_account_type, :message => "用户账户类型不可以重复!"
+
   
   ## 与用户账户表进行关联
   has_many :hwm_user_accounts
